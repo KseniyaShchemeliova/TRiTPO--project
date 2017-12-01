22aec9e309223bf6019b985f52e896a12a762ae5
Kseniya Shchemeliova <kseniya.shchamialiova@gmail.com>
2017-12-01 12:06:11 +0300

    add finaly project

---
+++ Code/konverter1/konverter1/Converter.cs
@@ -0,0 +1,15 @@
+﻿namespace konverter1
+{
+    public interface IConverting
+    {
+        double Convert(double amount, double fromRate, double toRate);
+    }
+
+    internal class Converter : IConverting
+    {
+        public double Convert(double amount, double fromRate, double toRate)
+        {
+            return fromRate / toRate * amount;
+        }
+    }
+}
